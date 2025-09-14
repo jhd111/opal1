6cb1741c15a97d7af09f6a2ebfdf49e2dce7b3b7
jhd111 <m.hanaan111@gmail.com>
2025-09-14 21:49:50 +0500

    new new changes

--- src/Base_url/Base_url.jsx
+++ src/Base_url/Base_url.jsx
@@ -1,5 +1,5 @@
-const BaseUrl="https://6c6d4e9bb12b.ngrok-free.app"
+// const BaseUrl="https://6c6d4e9bb12b.ngrok-free.app"
 
-// const BaseUrl="https://backend.opalinstitute.pk"
+const BaseUrl="https://backend.opalinstitute.pk"
 
 export default BaseUrl
